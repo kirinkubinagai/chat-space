59495be164737209ea6cdb7f9f1ffde1462c0eb9
柴田 大介 <oujirou6kg@gmail.com>
2016-11-21 16:32:17 +0900

    モデル再編集
    モデル再編集

--- db/migrate/20161121070840_create_users_groups.rb
+++ db/migrate/20161121070840_create_users_groups.rb
@@ -1,8 +1,8 @@
 class CreateUsersGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :users_groups do |t|
-      t.references :users,foreign_key: :user_id
-      t.references :chatgroups,foreign_key: :chat_group_id
+      t.references :user,foreign_key: :user_id
+      t.references :chat_group,foreign_key: :chat_group_id
       t.timestamps
     end
   end
