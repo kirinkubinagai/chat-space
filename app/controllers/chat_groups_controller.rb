c556cce9827ba7088cc5240575052a3ef19fac40
柴田 大介 <oujirou6kg@gmail.com>
2016-11-21 15:19:32 +0900

    controllerの編集
    コントローラーの編集

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -6,10 +6,19 @@ class ChatGroupsController < ApplicationController
   def new
   end
 
+  def create
+    Chat_group.create(name: create_params[:name],user_id: create_params[:user_id])
+  end
+
   def edit
   end
 
   def show
+  end
+
+  private
+  def create_params
+     params.permit(:name,:user_id)
   end
 
 end
