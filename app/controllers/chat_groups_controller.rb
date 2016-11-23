7eadf0eb5a87517ab1dcbb55c99b655b4a4e80c1
柴田 大介 <oujirou6kg@gmail.com>
2016-11-23 22:21:12 +0900

    インデント、及び名前などの変更
    インデント、及び名前などの変更

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -2,15 +2,15 @@ class ChatGroupsController < ApplicationController
 
   before_action :group,only: [:edit,:update]
   def index
-    @chat_groups=current_user.chat_group
+    @chat_groups = current_user.chat_groups
   end
 
   def new
-    @chat_group=ChatGroup.new
+    @chat_group = ChatGroup.new
   end
 
   def create
-    @chat_group=ChatGroup.new(create_params)
+    @chat_group = ChatGroup.new(chat_group_params)
     if @chat_group.save
       redirect_to action: :index
     else
@@ -30,7 +30,7 @@ class ChatGroupsController < ApplicationController
   end
 
   private
-  def create_params
+  def chat_group_params
      params.require(:chat_group).permit({user_ids:[]}, :name)
   end
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_group
-  validates :body,presence: true
+  validates :body, presence: true
 end
