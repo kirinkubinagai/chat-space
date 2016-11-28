96d59dba2deeef5ab4537726b50a455cbf01d032
柴田 大介 <oujirou6kg@gmail.com>
2016-11-28 17:29:36 +0900

    修正しました
    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,27 @@
 $(function() {
   function buildHTML(data) {
     var chat =
-      '<ul class="chat-message">'
-        '<li class = "chat-message__header">'
-          '<p class = "chat-message__name">'
-            data.name
-          '</p>'
-          '<p class = "chat-message__time">'
-            data.created_at
-          '</p>'
-        '</li>'
-        '<li class = "chat-message__body">'
-            data.body
-        '</li>'
-      '</ul>'
-    $("<div class='chat_body'>").append(chat)
+      '<ul class="chat-message">' +
+        '<li class = "chat-message__header">' +
+          '<p class = "chat-message__name">' +
+            data.name +
+          '</p>' +
+          '<p class = "chat-message__time">' +
+            data.created_at +
+          '</p>' +
+        '</li>' +
+        '<li class = "chat-message__body">' +
+            data.body +
+        '</li>' +
+      '</ul>' ;
+    return chat ;
   }
 
   $('.new-message').on('submit', function(e) {
     e.preventDefault();
-    var text_field = $("textarea")
-    var message = text_field.val();
-    var new_url = $("#new_message").attr("action")
-    var new_url_for_json = new_url+".json"
+    var message = $("textarea").val();
+    var new_url = $("#new_message").attr("action");
+    var new_url_for_json = new_url+".json";
     $.ajax({
       type: 'POST',
       url: new_url_for_json,
@@ -30,8 +29,10 @@ $(function() {
       dataType: 'json'
     })
     .done(function(data) {
-      buildHTML(data);
-      $("#message_body").val("")
+       html = buildHTML(data);
+      $('.chat-body').append(html);
+      console.log($('.chat-body'));
+      $("#message_body").val("");
     })
     .fail(function() {
       alert('error');
