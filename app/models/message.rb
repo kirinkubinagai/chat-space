552e7f737ea99fb92e24265eda2e0368ad0f960a
柴田 大介 <oujirou6kg@gmail.com>
2016-12-01 16:04:03 +0900

    バリデーションの編集
    バリデーションの編集

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_group
-  validates :body, presence: true
+  validates :body, presence: true, if: ->{image.blank?}
+  mount_uploader :image,ImageUploader
 end
