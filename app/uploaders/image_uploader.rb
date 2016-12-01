9332adc9eb8603303f5feead2db208c4e5f95818
柴田 大介 <oujirou6kg@gmail.com>
2016-12-01 17:48:05 +0900

    アップローダーの編集
    アップローダーの編集

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,28 +1,37 @@
-# app/uploaders/image_uploader.rb
-class ImageUploader < CarrierWave::Uploader::Base
+# encoding: utf-8
+
+class ImageUploader < CarrierWave::Uploader::Base  
+
+ # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
 
-  storage :file
+ # 画像の上限を700pxにする
+  process :resize_to_limit => [700, 700]
 
-  process convert: 'jpg'
+  # 保存形式をJPGにする
+  process :convert => 'jpg'
 
-  # 保存するディレクトリ名
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # thumb バージョン(width 400px x height 200px)
+  # サムネイルを生成する設定
   version :thumb do
-    process :resize_to_fit => [600, 300]
+    process :resize_to_limit => [300, 300]
   end
 
-  # 許可する画像の拡張子
+  # jpg,jpeg,gif,pngしか受け付けない
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  # 変換したファイルのファイル名の規則
+ # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
   def filename
-    "#{Time.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
   end
+
+ # ファイル名は日本語が入ってくると嫌なので、下記のようにしてみてもいい。
+ # 日付(20131001.jpgみたいなファイル名)で保存する
+  def filename
+    time = Time.now
+    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+    name.downcase
+  end
+
 end
