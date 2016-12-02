62a821e7d48ef5f373414f8ff71ad865c9de8ec0
柴田 大介 <oujirou6kg@gmail.com>
2016-12-02 12:26:28 +0900

    画像投稿機能ほぼ完成
    画像投稿機能ほぼ完成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,40 +1,92 @@
 $(function() {
   function buildHTML(data) {
-    var chat =
-      '<ul class="chat-message">' +
-        '<li class = "chat-message__header">' +
-          '<p class = "chat-message__name">' +
-            data.name +
-          '</p>' +
-          '<p class = "chat-message__time">' +
-            data.created_at +
-          '</p>' +
-        '</li>' +
-        '<li class = "chat-message__body">' +
-            data.body +
-        '</li>' +
-      '</ul>' ;
-    return chat ;
+    if (data.image.image.url == null){
+      var chat =
+        '<ul class="chat-message">' +
+          '<li class = "chat-message__header">' +
+            '<p class = "chat-message__name">' +
+              data.name +
+            '</p>' +
+            '<p class = "chat-message__time">' +
+              data.created_at +
+            '</p>' +
+          '</li>' +
+          '<li class = "chat-message__body">' +
+              data.body +
+          '</li>' +
+        '</ul>';
+    }else if(data.body != "" && data.image.image.url != null){
+      var image_data = data.image.image.url
+      var alt_data = image_data.replace(/\.jpg/,"")
+      var new_alt_data = alt_data.replace(/_/g," ")
+      var new_alt_data = new_alt_data.replace(/\/uploads\//,"")
+      var chat =
+        '<ul class="chat-message">' +
+          '<li class = "chat-message__header">' +
+            '<p class = "chat-message__name">' +
+              data.name +
+            '</p>' +
+            '<p class = "chat-message__time">' +
+              data.created_at +
+            '</p>' +
+          '</li>' +
+          '<li class = "chat-message__body">' +
+              data.body +
+          '</li>' +
+          '<div class= "chat_message_image">' +
+            '<br>' +
+            '<img src ="' + image_data + '"' + ' alt ="' + new_alt_data +'" class ="chat_img">'
+          '</div>' +
+        '</ul>';
+    }else if(data.body === "" && data.image.image.url != null) {
+      var image_data = data.image.image.url
+      var alt_data = image_data.replace(/\.jpg/,"")
+      var new_alt_data = alt_data.replace(/_/g," ")
+      var new_alt_data = new_alt_data.replace(/\/uploads\//,"")
+      var chat =
+        '<ul class="chat-message">' +
+          '<li class = "chat-message__header">' +
+            '<p class = "chat-message__name">' +
+              data.name +
+            '</p>' +
+            '<p class = "chat-message__time">' +
+              data.created_at +
+            '</p>' +
+          '</li>' +
+          '<div class= "chat_message_image">' +
+            '<br>' +
+            '<img src ="' + image_data + '"' + ' alt ="' + new_alt_data +'" class ="chat_img">'
+          '</div>' +
+        '</ul>';
+    }
+  return chat;
   }
 
-  $('.new-message').on('submit', function(e) {
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
+    var form = $("#new_message").get()[0];
+    var formData = new FormData(form);
     var message = $("textarea").val();
     var new_url = $("#new_message").attr("action");
     var new_url_for_json = new_url+".json";
     $.ajax({
       type: 'POST',
       url: new_url_for_json,
-      data: {message: {body: message}},
-      dataType: 'json'
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
     .done(function(data) {
       $('.chat-body').append(buildHTML(data));
       $("#message_body").val("");
-      $(".chat_submit").prop("disabled",false);
+      return false;
     })
     .fail(function() {
       alert('error');
     });
   });
 });
+
+
+// 複数の画像を投稿した時の条件分岐ができてない、redirectできない
