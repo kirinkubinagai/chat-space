7875580d42e0cd0056e06c48a8e225c918fdccbc
柴田 大介 <oujirou6kg@gmail.com>
2016-12-10 11:44:53 +0900

    不要と思われる箇所を削除
    不要と思われる箇所を削除

--- config/initializers/carrierWave.rb
+++ config/initializers/carrierWave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => ENV["S3_ACCESS_KEY_ID"],
       :aws_secret_access_key  => ENV['S3_SECRET_ACCESS_KEY'],
-      :region                 => ENV["S3_REGION"],
       :path_style             => true,
   }
 
